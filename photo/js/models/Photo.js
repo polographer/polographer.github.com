236627c41442391095fee1e0d57ce552008b0952
Polo Ornelas <me@poloornelas.mx>
2014-05-28 21:03:20 -0400

    changed photo url

--- photo/js/models/Photo.js
+++
@@ -1,6 +0,0 @@
-var App = App || {};
-App.Models = App.Models || {};
-
-App.Models.Photo = Backbone.Model.extend({
-	url: ""
-});
--- photo/js/models/PhotosCollection.js
+++
@@ -1,6 +0,0 @@
-var App = App || {};
-App.Models = App.Models || {};
-
-App.Models.PhotosCollection = Backbone.Collection.extend({ 
-	model:App.Models.Photo,
-});
